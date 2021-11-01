3c7548fc7e94883962c2c8138f23c21d7a002ac2
Marcos Toranzo <marcos.toranzo11@gmail.com>
2021-11-01 09:43:58 -0400

    Add dislikes to Routine entity. #30

--- src/routine.py
+++ src/routine.py
@@ -8,7 +8,7 @@ class Comment():
 
 
 class Routine():
-    def __init__(self, description: str, uploaded_by: User, likes: int, comments: list, tags: list, exercises: list):
+    def __init__(self, description: str, uploaded_by: User, likes: int, dislikes: int, comments: list, tags: list, exercises: list):
         '''
         Initializes a new instance of Routine that contains the [Exercise]s to do. Also contains the total workout table
         based on the individual workout tables from the exercises.
@@ -17,6 +17,7 @@ class Routine():
             description: short description about the routine.
             uploaded_by: [User] that uploaded the routine.
             likes: likes the routines has had so far. Must be an [int].
+            dislikes: dislikes the routines has had so far. Must be an [int].
             comments: comments and feedbacks from users. Must be a [list] of [Comment]s.
             tags: tags related to the routine. Must be a [list] of [str]s.
             exercises: exercises that comprehen the routines. Must be a [list] of [Exercise]s.
@@ -24,6 +25,7 @@ class Routine():
         self.description = description
         self.uploaded_by = uploaded_by
         self.likes = likes
+        self.dislikes = dislikes
         self.comments = comments
         self.tags = tags
         self.exercises = exercises
