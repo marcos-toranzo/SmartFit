75e2ddfd6abeb45b0319c89c942b17f94433452f
Marcos Toranzo <marcos.toranzo11@gmail.com>
2021-12-16 00:38:25 -0500

    Add setup.py file #83

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='requirements',
+    description='Install requirements',
+    license="GPL-3.0",
+    author='Marcos Toranzo Alfonso',
+    install_requires=['pytest', 'fastapi', 'uvicorn[standard]'],
+)
